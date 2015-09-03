e440b52ae53c6af326adf81e032e11c30cc1d65a
Emil Persson <emil.n.persson@gmail.com>
2015-09-03 19:23:45 +0200

    Don't require sqljocky to be externally imported

--- lib/src/drivers/my_sql_driver.dart
+++ lib/src/drivers/my_sql_driver.dart
@@ -3,7 +3,23 @@ part of trestle.drivers;
 class MySqlDriver extends SqlDriver {
   final sqljocky.ConnectionPool _connection;
 
-  MySqlDriver(sqljocky.ConnectionPool this._connection);
+  MySqlDriver({String host: 'localhost',
+  int port: 3306,
+  String user,
+  String password,
+  String db,
+  int max: 5,
+  int maxPacketSize: 16 * 1024 * 1024,
+  bool useSSL: false})
+      : _connection = new sqljocky.ConnectionPool(
+      host: host,
+      port: port,
+      user: user,
+      password: password,
+      db: db,
+      max: max,
+      maxPacketSize: maxPacketSize,
+      useSSL: useSSL);
 
   Future connect() async {
   }
@@ -13,7 +29,8 @@ class MySqlDriver extends SqlDriver {
   }
 
   Stream<Map<String, dynamic>> execute(String query, List variables) async* {
-    sqljocky.Results results = await (await _connection.prepare(query)).execute(variables);
+    sqljocky.Results results = await (await _connection.prepare(query)).execute(
+        variables);
     Iterable<String> fieldNames = results.fields.map((f) => f.name);
     yield* results.map((row) => new Map.fromIterables(fieldNames, row));
   }
